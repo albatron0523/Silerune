94e9f86e0d89d50f6c45686cedaf9f85627c4fbc
albatron <dora0955633238@gmail.com>
2026-04-05 01:08:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // 關鍵：這行確保你的 CSS/JS 路徑正確，不會找不到檔案
   base: '/Silerune/', 
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  }
 })
