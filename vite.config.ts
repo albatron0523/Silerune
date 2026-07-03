d8d57943035977c8e722c76f872bc738f558d374
albatron <dora0955633238@gmail.com>
2026-07-04 01:30:29 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
+export default defineConfig(() => {
   return {
-    // --- 加入這一行，這是解決空白頁面的關鍵 ---
-    base: '/Silerune/', 
-    // ---------------------------------------
-    
+    // 💡 把它改成 './'，這代表使用相對路徑
+    // 這樣不管你的 GitHub 專案叫什麼名字，網頁都能百分之百精準抓到同目錄下的資源！
+    base: './', 
+
     plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
@@ -21,6 +17,7 @@ export default defineConfig(({mode}) => {
     },
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
     },
   };
 });
