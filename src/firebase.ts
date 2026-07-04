a748ebcf2fd8abbbd50dea1b2d1cb82e046d8dac
albatron <dora0955633238@gmail.com>
2026-07-04 13:59:58 +0800

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "...",              // 换成 silerune-ee33e 的
-  authDomain: "silerune-ee33e.firebaseapp.com",
-  projectId: "silerune-ee33e",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyBUVbe9YAS2qU01DcIiTVTfHJ6rQUzTluk",
+  authDomain: "v2345-66433.firebaseapp.com",
+  projectId: "v2345-66433",
+  storageBucket: "v2345-66433.firebasestorage.app",
+  messagingSenderId: "326140874923",
+  appId: "1:326140874923:web:594fcda27b3486e682e7ee"
 };
 
 // 初始化 Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const docRef = doc(db, "silerune", "data");
+export const docRef = doc(db, "websites", "homepage");
